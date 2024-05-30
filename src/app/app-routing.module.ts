98e20d91a39dee51ead4ab4fadc32fdcfdc3da35
moonymel <musumeci.me@gmail.com>
2024-05-30 17:06:49 +0200

    [UPDATE] setting routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+// in questo file setto le rotte della single page app
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
-const routes: Routes = [];
+// definisco le rotte come array di oggetti che hanno tutti la stessa sintassi (path e component)
+const routes: Routes = [
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
